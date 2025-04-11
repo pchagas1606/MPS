e666b41633dac01eb3cca1374716de6ea9149849
Iago Guedes <iagoalbguedes@gmail.com>
2025-04-10 23:29:40 -0300

    Alteração na Main

--- mps_notas_back/cmd/api/main.go
+++ mps_notas_back/cmd/api/main.go
@@ -7,7 +7,8 @@ import (
 	"mps_notas_back/internal/buisness/middleware"
 	"mps_notas_back/internal/buisness/router"
 	"mps_notas_back/internal/buisness/service"
-	"mps_notas_back/internal/infra/repository"
+	"mps_notas_back/internal/infra/database"
+	"mps_notas_back/internal/infra/factory"
 
 	"net/http"
 )
@@ -16,14 +17,19 @@ func main() {
 	// Carregar configurações
 	cfg := config.New()
 
+	db, err := database.GenConn("internal/infra/database/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
 	// Inicializar repositórios
-	userRepo := repository.NewUserRepository()
-
+	userRepo := factory.UserDAOSql{}.GenFactory(db)
+	taskRepo := factory.TaskDAOSql{}.GenFactory(db)
 	// Inicializar serviços
 	userService := service.NewUserService(userRepo)
+	taskService := service.NewTaskService(taskRepo)
 
 	// Configurar router
-	r := router.New(userService)
+	r := router.New(userService, taskService)
 
 	// Aplicar middlewares, desativado CORS por enquanto, pois não há mais necessidade
 	//corsConfig := middleware.DefaultCORSConfig()
