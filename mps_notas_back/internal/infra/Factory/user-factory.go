673b67a6511121bd170a626f94f48db4bf0da10f
pchagas1606 <pedrochagas876@gmail.com>
2025-04-10 23:06:02 -0300

    Alteração no user-factory

--- mps_notas_back/internal/infra/Factory/user-factory.go
+++ mps_notas_back/internal/infra/Factory/user-factory.go
@@ -1,7 +1,23 @@
 package factory
 
-import "mps_notas_back/internal/infra/model"
+import (
+	"database/sql"
+	"mps_notas_back/internal/infra/model"
+	"mps_notas_back/internal/infra/repository"
+)
 
 type UserFactory interface {
-	GenFactory(string) *model.UserDAO
+	GenFactory() repository.UserRepository
+}
+
+type UserDAOInMemory struct{}
+
+func (UserDAOInMemory) GenFactory() repository.UserRepositoryInMemory {
+	return repository.UserRepositoryInMemory{Users: []model.UserDAO{}, CurrentID: 1}
+}
+
+type UserDAOSql struct{}
+
+func (UserDAOSql) GenFactory(db *sql.DB) repository.UserRepository {
+	return repository.UserRepositorySql{DB: db}
 }
